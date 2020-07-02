f2f66c0a7e50a562776210aff17a186d11c8f22a
yuangezhizao <root@yuangezhizao.cn>
2020-07-02 20:25:24 +0800

    WebStorm Reformat

--- src/test.js
+++ src/test.js
@@ -3,7 +3,7 @@ var Gitment = Gitment || require('./gitment')
 const config = window.config
 
 if (!config) {
-  throw new Error('You need your own config to run this test.')
+    throw new Error('You need your own config to run this test.')
 }
 
 const gitment = new Gitment(config)
@@ -13,5 +13,6 @@ gitment.render('container')
 window.gitment = gitment
 
 try {
-  window.http = require('./utils').http
-} catch (e) {}
+    window.http = require('./utils').http
+} catch (e) {
+}
--- src/utils.js
+++ src/utils.js
@@ -1,88 +1,88 @@
-import { LS_ACCESS_TOKEN_KEY } from './constants'
+import {LS_ACCESS_TOKEN_KEY} from './constants'
 
 export const isString = s => toString.call(s) === '[object String]'
 
 export function getTargetContainer(container) {
-  let targetContainer
-  if (container instanceof Element) {
-    targetContainer = container
-  } else if (isString(container)) {
-    targetContainer = document.getElementById(container)
-  } else {
-    targetContainer = document.createElement('div')
-  }
+    let targetContainer
+    if (container instanceof Element) {
+        targetContainer = container
+    } else if (isString(container)) {
+        targetContainer = document.getElementById(container)
+    } else {
+        targetContainer = document.createElement('div')
+    }
 
-  return targetContainer
+    return targetContainer
 }
 
 export const Query = {
-  parse(search = window.location.search) {
-    if (!search) return {}
-    const queryString = search[0] === '?' ? search.substring(1) : search
-    const query = {}
-    queryString.split('&')
-      .forEach(queryStr => {
-        const [key, value] = queryStr.split('=')
-        if (key) query[key] = value
-      })
+    parse(search = window.location.search) {
+        if (!search) return {}
+        const queryString = search[0] === '?' ? search.substring(1) : search
+        const query = {}
+        queryString.split('&')
+            .forEach(queryStr => {
+                const [key, value] = queryStr.split('=')
+                if (key) query[key] = value
+            })
 
-    return query
-  },
-  stringify(query, prefix = '?') {
-    const queryString = Object.keys(query)
-      .map(key => `${key}=${encodeURIComponent(query[key] || '')}`)
-      .join('&')
-    return queryString ? prefix + queryString : ''
-  },
+        return query
+    },
+    stringify(query, prefix = '?') {
+        const queryString = Object.keys(query)
+            .map(key => `${key}=${encodeURIComponent(query[key] || '')}`)
+            .join('&')
+        return queryString ? prefix + queryString : ''
+    },
 }
 
 function ajaxFactory(method) {
-  return function(apiPath, data = {}, base = 'https://api.github.com') {
-    const req = new XMLHttpRequest()
-    const token = localStorage.getItem(LS_ACCESS_TOKEN_KEY)
+    return function (apiPath, data = {}, base = 'https://api.github.com') {
+        const req = new XMLHttpRequest()
+        const token = localStorage.getItem(LS_ACCESS_TOKEN_KEY)
 
-    let url = `${base}${apiPath}`
-    let body = null
-    if (method === 'GET' || method === 'DELETE') {
-      url += Query.stringify(data)
-    }
-
-    const p = new Promise((resolve, reject) => {
-      req.addEventListener('load', () => {
-        const contentType = req.getResponseHeader('content-type')
-        const res = req.responseText
-        if (!/json/.test(contentType)) {
-          resolve(res)
-          return
+        let url = `${base}${apiPath}`
+        let body = null
+        if (method === 'GET' || method === 'DELETE') {
+            url += Query.stringify(data)
         }
-        const data = req.responseText ? JSON.parse(res) : {}
-        if (data.message) {
-          reject(new Error(data.message))
-        } else {
-          resolve(data)
+
+        const p = new Promise((resolve, reject) => {
+            req.addEventListener('load', () => {
+                const contentType = req.getResponseHeader('content-type')
+                const res = req.responseText
+                if (!/json/.test(contentType)) {
+                    resolve(res)
+                    return
+                }
+                const data = req.responseText ? JSON.parse(res) : {}
+                if (data.message) {
+                    reject(new Error(data.message))
+                } else {
+                    resolve(data)
+                }
+            })
+            req.addEventListener('error', error => reject(error))
+        })
+        req.open(method, url, true)
+
+        req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview, application/vnd.github.html+json')
+        if (token) {
+            req.setRequestHeader('Authorization', `token ${token}`)
         }
-      })
-      req.addEventListener('error', error => reject(error))
-    })
-    req.open(method, url, true)
+        if (method !== 'GET' && method !== 'DELETE') {
+            body = JSON.stringify(data)
+            req.setRequestHeader('Content-Type', 'application/json')
+        }
 
-    req.setRequestHeader('Accept', 'application/vnd.github.squirrel-girl-preview, application/vnd.github.html+json')
-    if (token) {
-      req.setRequestHeader('Authorization', `token ${token}`)
+        req.send(body)
+        return p
     }
-    if (method !== 'GET' && method !== 'DELETE') {
-      body = JSON.stringify(data)
-      req.setRequestHeader('Content-Type', 'application/json')
-    }
-
-    req.send(body)
-    return p
-  }
 }
 
 export const http = {
-  get: ajaxFactory('GET'),
-  post: ajaxFactory('POST'),
-  delete: ajaxFactory('DELETE'),
-  put: ajaxFactory('PUT'),
+    get: ajaxFactory('GET'),
+    post: ajaxFactory('POST'),
+    delete: ajaxFactory('DELETE'),
+    put: ajaxFactory('PUT'),
 }
